1fbcb311f51d8edfefa003503e00771dff9e4f5c
Divyansh Joshi <65797078+divyansh-joshi@users.noreply.github.com>
2026-09-17 10:01:27 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1]*len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            take = nums[i]
+            if i-2 >= 0:
+                take += dp[i-2]
+            not_take = dp[i-1]
+            dp[i] = max(take, not_take)
+        return dp[len(nums)-1]
